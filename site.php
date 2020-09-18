d33ab9a0145d3ef368cf87a3641b5ace7b09c14c
kakarotto1994 <filipericardo.britosilva@gmail.com>
2020-09-18 19:35:38 -0300

    Alterar senha

--- site.php
+++ site.php
@@ -678,8 +678,92 @@ $app->get("/profile/orders/:idorder", function($idorder) {
 	$page->setTpl("profile-orders-detail", [
 		'order'=>$order->getValues(),
 		'cart'=>$cart->getValues(),
-		'products'=>$cart->getOrderProducts()
+		'products'=>$cart->getProducts()
 	]);
+
+});
+
+$app->get("/profile/change-password",function(){
+
+	User::verifyLogin(false);
+
+	$page = new Page();
+
+	$page->setTpl("profile-change-password", [
+		"changePassError"=>User::getError(),
+		"changePassSuccess"=>User::getSuccess()
+	]);
+
+});
+
+$app->post("/profile/change-password",function(){
+
+	User::verifyLogin(false);
+
+	if(!isset($_POST['current_pass']) || $_POST['current_pass'] === '') {
+
+		User::setError("Digite a Senha atual: ");
+
+		header('Location: /profile/change-password');
+		exit;
+
+	}
+
+	if(!isset($_POST['new_pass']) || $_POST['new_pass'] === '') {
+
+		User::setError("Digite a nova Senha ");
+
+		header('Location: /profile/change-password');
+		exit;
+
+	}
+
+	if(!isset($_POST['new_pass_confirm']) || $_POST['new_pass_confirm'] === '') {
+
+		User::setError("Confirme a nova Senha");
+
+		header('Location: /profile/change-password');
+		exit;
+
+	}
+
+	if(($_POST['new_pass']) === $_POST['current_pass']) {
+
+		User::setError("Sua Nova senha deve ser diferente da atual. ");
+
+		header('Location: /profile/change-password');
+		exit;
+
+	}
+
+	if(($_POST['new_pass']) !== $_POST['new_pass_confirm']) {
+
+		User::setError("Nova senha digitada não confere. ");
+
+		header('Location: /profile/change-password');
+		exit;
+
+	}
+
+	$user = User::getFromSession();
+
+	if(!password_verify($_POST['current_pass'], $user->getdespassword())) {
+
+		User::setError("Senha atual invalida.");
+
+		header('Location: /profile/change-password');
+		exit;
+
+	}
+	
+	$user->setdespassword($user->getPasswordHash($_POST['new_pass']));
+
+	$user->update();
+
+	User::setSuccess("Senha Alterada com sucesso");
+
+	header('Location: /profile/change-password');
+	exit;
 
 });
 
